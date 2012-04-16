0a1cb27ee8f51648e3f521d0b0a0493909f7371b
Scotty <vader426@gmail.com>
2012-04-16 04:52:31 -0300

    Made appropriate changes to methods that required Territory accessor methods and finished attack()

--- Player.java
+++ Player.java
@@ -7,49 +7,50 @@
  * 
  */
 import java.util.*;
+import java.util.Collections;
 
 public class Player {
-	
+
 	private ArrayList<ArrayList<Integer>> territories;
 	private ArrayList<Integer> cards;
 	private boolean hasConquered;
-	
+
 	//Constructor
 	public Player(){
 		territories = new ArrayList<ArrayList<Integer>>();
 		cards = new ArrayList<Integer>();
 		hasConquered = false;
 	}
-	
+
 	public ArrayList<ArrayList<Integer>> getTerritories() {
 		return territories;
 	}
-	
+
 	public void setTerritories(ArrayList<ArrayList<Integer>> territories) {
 		this.territories = territories;
 	}
-	
+
 	public ArrayList<Integer> getCards() {
 		return cards;
 	}
-	
+
 	public void setCards(ArrayList<Integer> cards) {
 		this.cards = cards;
 	}
-	
+
 	public boolean isHasConquered() {
 		return hasConquered;
 	}
-	
+
 	public void setHasConquered(boolean hasConquered) {
 		this.hasConquered = hasConquered;
 	}
-	
+
 	public int troopsAt(Territory T){
 		return territories.get(0).get(0);//TODO
 	}
-	
-	
+
+
 	/*This method returns true if the player has a complete set of cards and false if they don't.
 	 *A complete set is 3 of any one card or 1 of all three cards. 
 	 */
@@ -77,7 +78,7 @@ public class Player {
 			return false;
 		}
 	}
-	
+
 	/* Returns whether the player has a set of three identical cards.
 	 */
 	public boolean hasTriplet(){
@@ -95,7 +96,7 @@ public class Player {
 		}
 		return (ones >= 3 || twos >= 3 || threes >= 3);
 	}
-	
+
 	/* Returns whether the player has one of each type of card.
 	 */
 	public boolean hasMixedSet(){
@@ -113,7 +114,7 @@ public class Player {
 		}
 		return (ones > 0 && twos > 0 && threes > 0);
 	}
-	
+
 	/*Asks the player if they want to turn in cards, although I (Trey) don't think this method should be here. It would
 	 * probably be better in GameManager.
 	 */
@@ -125,85 +126,120 @@ public class Player {
 		else
 			return false;//TODO to be implemented once the UI is implemented.
 	}
-	
-	
+
+
 	/* Turns in cards and returns the number of reinforcements gotten. It asks the GameManager what the current reward for
 	 * turning in cards. Possibly handle the user choosing which combination to turn in if more than one is possible.
 	 */
 	public int turnInCards(){
 		return 0;//TODO
 	}
-	
+
 	/*Helper method for turnInCards()
 	 */
 	private int numReinforcements(){
 		return 0;//TODO
 	}
-	
+
 	/* This method comprises the entire process of placing reinforcements.
 	 */
 	public void reinforceProcess(){
 		//TODO
 	}
-	
+
 	/*This method returns a jagged matrix representing where the user wants to place reinforcements
 	 */
 	public ArrayList<ArrayList<Integer>> askReinforcemnts(){//TODO
 		return null;
 	}
-	
+
 	/*
 	 * Returns a territory in which to place a reinforcement at the beginning of the game.
 	 */
 	public Territory askInitReinforce(){
 		return null; //TODO
 	}
-	
+
 	//Places a single troop in a territory t.
 	public void placeReinforcement(Territory t){
-		//TODO
+		t.reinforce();
 	}
-	
+
 	//Places multiple troops in a territory t.
 	public void placeReinforcements(Territory t, int num){
-		//TODO
+		t.reinforce(num);
 	}
-	
+
 	//This method encompasses the entire attack procedure. Returns whether the player won at least one battle.
 	public boolean attackProcess(){
 		return false;//TODO
 	}
-	
+
 	//The method returns whether or not a player has the ability to attack.
 	public boolean canAttack(){
 		return false;//TODO
 	}
-	
+
 	//attack a specific territory from another territory with a certain number of troops. Returns whether the player won.
 	public boolean attack(Territory from, Territory to, int troops){
-		return false;//TODO
+		Random r = new Random();
+		int attackerLoss = 0, defenderLoss = 0;
+		int[] attackerRolls = new int[troops];
+		int[] defenderRolls = null;
+		if(to.getTroops() ==1){
+			defenderRolls = new int[1];
+		}
+		else if (to.getTroops() > 1){
+			defenderRolls = new int[2];
+		}
+		for(int i = 0; i < troops; i++){
+			attackerRolls[i] = r.nextInt(5) + 1;
+		}
+		for(int j = 0; j < defenderRolls.length; j++){
+			defenderRolls[j] = r.nextInt(5) + 1;
+		}
+		Arrays.sort(attackerRolls);
+		Arrays.sort(defenderRolls);
+		for(int n = 1; n <= defenderRolls.length; n++){
+			if(attackerRolls[attackerRolls.length - n] > defenderRolls[defenderRolls.length - n]){
+				defenderLoss++;
+			}
+			else{
+				attackerLoss++;
+			}
+		}
+		
+		from.remove(attackerLoss);
+		to.remove(defenderLoss);
+		if(to.getTroops() < 1){
+			return true;
+		}
+		else{
+			return false;
+		}
 	}
-	
+
 	//Encompasses the entire moving procedure
 	public void moveProcess(){//TODO
-		
+
 	}
-	
+
 	//returns whether or not the player has the ability to move troops
 	public boolean canMove(){
 		return false;//TODO
 	}
-	
+
 	public void move(Territory from, Territory to, int num){
-		//TODO
+		from.remove(num);
+		to.reinforce(num);
 	}
-	
+
 	//adds a card to the players list if they have conquered this turn. To be called at the end of a turn
 	public void collectCard(){
 		int card = (int)(Math.random()*3 +1);
 		cards.add(card);
 	}
-	
+
 	//adds all the cards that belong to a player to another players list. To be called when a player is defeated.
 	public void collectCards(Player p){
 		cards.addAll(p.getCards());
