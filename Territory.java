b758046d85f2e2446d592678b4f1c64308d96bb4
Scotty <vader426@gmail.com>
2012-04-16 04:49:46 -0300

    Finished Territory implementation

--- Territory.java
+++ Territory.java
@@ -1,13 +1,43 @@
+
 public class Territory {
 	
 	private String name;
+	private int troops;
 	
-	public Territory(String n) {
+	public Territory (String n){
 		name = n;
 	}
 	
+	public void reinforce(int n){
+		troops = troops + n;
+	}
+	
+	public void reinforce(){
+		troops++;
+	}
+	
+	public void remove(int n){
+		troops = troops - n;
+	}
+	
+	public void remove(){
+		troops--;
+	}
+
 	public String getName() {
 		return name;
 	}
 
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getTroops() {
+		return troops;
+	}
+
+	public void setTroops(int troops) {
+		this.troops = troops;
+	}
+	
 }
