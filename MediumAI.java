319c834f4616199bb9b9baf6a5424eb8aeb4f821
Scotty <vader426@gmail.com>
2012-05-02 11:48:34 -0300

    Made it so mediumAI attacks from strongest territory to weakest neighbor

--- MediumAI.java
+++ MediumAI.java
@@ -1,48 +1,104 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Random;
 
 
-public class MediumAI extends ComputerPlayer {
-
-	public MediumAI(GameManager gm) {
+public class MediumAI extends ComputerPlayer{
+	
+	Random r;
+	public MediumAI(GameManager gm){
 		super(gm);
-		// TODO Auto-generated constructor stub
+		r = new Random();
 	}
-
-	@Override
+	
 	public HashMap<Territory, Integer> reinforceProcess() {
-		// TODO Auto-generated method stub
-		return null;
+		//TODO
+		//generates a hashmap of territories and their associate reinforcements
+		HashMap reinforcements = new HashMap<Territory, Integer>();
+		while(this.remainingReinforcements > 0){
+			reinforcements.put(t,)
+			
+		}
 	}
 
-	@Override
+	
 	protected ArrayList<Territory> askReinforcements(int numReinforcements) {
-		// TODO Auto-generated method stub
-		return null;
+		//generates a list of territories that the AI wants to reinforce
+		if(this.hasCardSet() == true){
+			this.turnInCards();
+		}
+		
+		int i = r.nextInt(this.territories.size());
+		ArrayList<Territory> t = new ArrayList<Territory>();
+		for(int j = 0; j < i && j < numReinforcements; j++){
+			t.add(this.territories.get(j));
+		}
+		return t;
 	}
 
-	@Override
+	
 	public Territory askInitReinforce() {
-		// TODO Auto-generated method stub
-		return null;
+		// gets a random territory to reinforce at the beginning of the game
+		Territory t = null;
+		int i = r.nextInt(manager.getBoard().getTerritories().size());
+		
+		do{
+			t = manager.getBoard().getTerritories().get(i);
+		}
+		while(t.getOwner() != null);
+		return t;
 	}
 
-	@Override
-	public Object[] attackProcess() {
-		// TODO Auto-generated method stub
-		return null;
+	
+	public boolean attackProcess() {
+		//comprises the entire attack process
+		return manager.attack(this.bestStage(), this.bestTarget(this.bestStage()), (this.bestStage().getTroops() -1));
 	}
 
-	@Override
-	public Object[] moveProcess() {
-		// TODO Auto-generated method stub
-		return null;
+	
+	public void moveProcess() {
+		//makes a random troop movement (random # of troop between 2 random territories)
+		
+		int i = r.nextInt(this.getTerritories().size());
+		Territory from = this.getTerritories().get(i);
+		int j = r.nextInt(this.getTerritories().size());
+		Territory to;
+		if(i != j){
+			to = this.getTerritories().get(j);
+		}
+		else{
+			j++;
+			to = this.getTerritories().get(j);
+		}
+		this.move(from, to, r.nextInt(from.getTroops()-1));
 	}
-
-	@Override
-	public Territory fortifyProcess() {
-		// TODO Auto-generated method stub
-		return null;
+	
+	public Territory bestTarget(Territory t){
+		int min = 1000;
+		Territory best = null;
+		ArrayList<Territory> connect = (ArrayList<Territory>) manager.getBoard().getConnections(t);
+		for(int j = 1; j < connect.size(); i++){
+			if(connect.get(j).getTroops() < min && connect.get(j).getOwner() != this);
+			min = connect.get(j).getTroops();
+			best = connect.get(j);
+		}
+		return best;
 	}
-
+	
+	public Territory bestStage(){
+		//gets the territory with the most troops
+		int max = 0;
+		Territory best = null;
+		for(int i = 0; i < this.territories.size(); i++){
+			if(this.territories.get(i).getTroops() > max){
+				max = this.territories.get(i).getTroops();
+				best = this.territories.get(i);
+			}
+		}
+		return best;
+	}
+	
+	public boolean continueAttack(){
+		return true;
+	}
 }
