7c4e394598a4fca467e2a862a9fc967531e017eb
Nick <axel3200@gmail.com>
2012-05-02 10:56:14 -0400

    main medium ai

---
+++ MediumAI2.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+public class MediumAI extends ComputerPlayer {
+
+	public MediumAI(GameManager gm) {
+		super(gm);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public HashMap<Territory, Integer> reinforceProcess() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected ArrayList<Territory> askReinforcements(int numReinforcements) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Territory askInitReinforce() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object[] attackProcess() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object[] moveProcess() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Territory fortifyProcess() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean continueAttack(int remaining, Object[] attack) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
