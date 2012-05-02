23625e5bd1526b5849bb80c58c849acd15796bdc
Nick <axel3200@gmail.com>
2012-05-02 09:00:53 -0400

    adding skeleton ai

---
+++ AdvancedAI.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+public class AdvancedAI extends ComputerPlayer {
+
+	public AdvancedAI(GameManager gm) {
+		super(gm);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public HashMap<Territory, Integer> reinforceProcess() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected ArrayList<Territory> askReinforcements(int numReinforcements) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Territory askInitReinforce() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean attackProcess() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void moveProcess() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Territory fortifyProcess() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
