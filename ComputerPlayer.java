b1121c8f5f25c524479ad86279debbbbe9284c38
Scotty <vader426@gmail.com>
2012-05-03 19:40:01 -0300

    Commented

--- ComputerPlayer.java
+++ ComputerPlayer.java
@@ -1,4 +1,4 @@
-
+//Class ComputerPlayer: Abstract superclass for the three AIs with a standard contructor
 public abstract class ComputerPlayer extends Player{
 
 	public ComputerPlayer(GameManager gm) {
